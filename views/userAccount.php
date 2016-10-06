469d2fac756dff7ed2988a6818a2026671903a44
htrevino29 <htrevino29@gmail.com>
2016-10-06 11:59:14 -0500

    added php to userAccount

--- views/userAccount.php
+++ views/userAccount.php
@@ -1,19 +1,12 @@
-<!-- <div class="container">	
-	<div class="col-sm-6 col-sm-offset-5">
-		<h3>User Info<h3>
-		<h5>username:</h5>
-		<h5>email:</h5>
-		<button type="button" class="btn btn-primary">Edit Profile</button>
-	</div>
-	<div class="col-sm-6 col-sm-offset-5">
-		<h3>Your Ads<h3>
-		<div class="text-centered yourAds">
-			<h5>ad</h5>
-			<h5>ad</h5>
-		</div>
-		<button type="button" class="btn btn-primary">Create Ad</button>
-	</div>	
-</div> -->
+<?php
+require_once __DIR__ . "/../utils/Auth.php";
+
+if (!Auth::check()) {
+	header("Location: http://adlister.dev/login");
+	die;
+}
+
+?>
 <br><br>
 <div class="container well span6 col-sm-4 col-sm-offset-4">
 	<div class="row">
