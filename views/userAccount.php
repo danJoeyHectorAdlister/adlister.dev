4ecbf4150d507f5dcd202150f2506806aaace342
htrevino29 <htrevino29@gmail.com>
2016-10-07 09:19:33 -0500

    sizing edits on show.php divs with text.
    working on modal for userAccount.php
    
               seeder had a few more typos.
    ------rerun add migration/seed to update your tables.--------

--- views/userAccount.php
+++ views/userAccount.php
@@ -48,6 +48,42 @@ if (!Auth::check()) {
             <button type="button" class="btn btn-primary">Edit Ads</button>
         </div>  
 	</div>
-
 </div>
+
+
+<h1 class="col-sm-12 text-center">fucking modal</h1>
+<!-- Button trigger modal -->
+<div class="container">
+  <button type="button" class="btn btn-primary btn-lg col-sm-4 col-sm-offset-4" data-toggle="modal" data-target="#myModal">
+   Launch demo modal
+  </button>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+ <div class="modal-dialog" role="document">
+   <div class="modal-content">
+     <div class="modal-header">
+       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+         <span aria-hidden="true">&times;</span>
+       </button>
+       <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+     </div>
+     <div class="modal-body">
+       ...
+     </div>
+     <div class="modal-footer">
+       <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       <button type="button" class="btn btn-primary">Save changes</button>
+     </div>
+   </div>
+ </div>
+</div>
+
+
+
+
+
+
+
 
