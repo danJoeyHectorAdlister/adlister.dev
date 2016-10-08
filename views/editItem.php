75b4a33673e72a966d7413f8c76f3dc4fb976ffa
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-10-08 15:36:35 -0500

    Now able to edit item on editItem page

--- views/editItem.php
+++ views/editItem.php
@@ -6,83 +6,50 @@ require_once __DIR__ . "/../models/Ad.php";
 
 
 // receives info from form
-$title = Input::get('title');
+$name = Input::get('name');
 $price = Input::get('price');
 $description = Input::get('description');
 $pic = Input::get('pic');
 $itemId = $_REQUEST['id'];
-// $ad = Model::find($id);
-
-// findItemByUserId($_SESSION["LOGGED_IN_ID"]);
 
 // places info from form into User database. Also retrieves user session id. If info does not match current table info, then table is updated
-if( !empty($title) && !empty($price) && !empty($description) && !empty($pic) ) {
+if( !empty($name) && !empty($price) && !empty($description) ) {
+	$imageUrl = saveUploadedImage('pic');
 	$ad = new Ad;
-	$ad->title = $title;
+	$ad->name = $name;
 	$ad->price = $price;
 	$ad->description = $description;
-	$ad->pic = $pic;
+	$ad->image_url = $imageUrl;
 	$ad->id = $itemId;
 	$ad->save();
 } else {
 	// var_dump("Invalid Parameters");
 }
 
-// variables created to represent current user info
-$ad = $itemId;
-var_dump($ad);
-// $title = $ad->attributes['title'];
-var_dump($_REQUEST);
-var_dump($_SESSION);
-// $username = $user->attributes['username'];
-// $email = $user->attributes['email'];
-// $name = $user->attributes['name'];
-
+// variables created to represent current ad info
+$ad = Ad::find($_REQUEST['id']);
+$currentName = $ad->attributes['name'];
+$currentPrice = $ad->attributes['price'];
+$currentDescription = $ad->attributes['description'];
+$currentPic = $ad->attributes['image_url'];
 
 ?>
 
 
-
-
-<!-- <form method="POST">				
-	<div class="form-group">
-		<label>Name</label>
-		<input class="form-control" name="name" placeholder="Name" value="<?php echo $name ?>">
-	</div>
-	<div class="form-group">
-		<label>User Name</label>
-		<input class="form-control" name="userName" placeholder="Username" value="<?php echo $username ?>">
-	</div>
-	<div class="form-group">
-		<label>Email address</label>
-		<input type="email" class="form-control" name="email" id="InputEmail" placeholder="Email" value="<?php echo $email ?>">
-	</div>
-	<div class="form-group">
-		<label>Password</label>
-		<input type="password" class="form-control" name="password" id="InputPassword" placeholder="Password" value="echo here">
-	</div>
-	<div class="form-group">
-		<label>Verify Password</label>
-		<input type="password" class="form-control" name="verifyPassword" id="VerifyPassword" placeholder="Password" value="echo here">
-	</div>
-	<button type="submit" class="btn btn-primary">Submit</button>
-</form> -->
-
-
 <div class="container col-sm-4 col-sm-offset-4">
-	<form method="POST" action="" id="editItem">	
+	<form method="POST" action="" id="editItem" enctype="multipart/form-data">	
 		<div class="form-group">
 			<label>Title</label>
-			<input  class="form-control" placeholder="Title" name="title">
+			<input  class="form-control" placeholder="Title" name="name" value="<?= $currentName ?>">
 		</div>
 		<div class="form-group">
 			<label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
 			<div class="input-group">
 				<div class="input-group-addon">$</div>
-				<input type="text" class="form-control" id="exampleInputAmount" placeholder="Amount" name="price">	  			
+				<input type="text" class="form-control" id="exampleInputAmount" placeholder="Amount" name="price" value="<?= $currentPrice ?>">	  			
 			</div>
 			<br>
-			<textarea class="form-control" rows="3" placeholder="Description" name="description"></textarea>
+			<textarea class="form-control" rows="3" placeholder="Description" name="description"><?= $currentDescription ?></textarea>
 		</div>
 		<div>
 		<h5>Select image for Ad.</h5>
@@ -92,3 +59,6 @@ var_dump($_SESSION);
 		<button type="submit" class="btn btn-primary">Update</button>
 	</form>
 </div>
+
+
+
